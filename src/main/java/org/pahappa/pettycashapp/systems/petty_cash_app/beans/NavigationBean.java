5ccda7c67b87e7451084c73299bbaf93d3cc448e
Arube Joshua <arubejoshua85@gmail.com>
2024-07-11 08:14:41 +0300

    Admin Pages, requisitions' sections and budget line sections

---
+++ src/main/java/org/pahappa/pettycashapp/systems/petty_cash_app/beans/NavigationBean.java
@@ -0,0 +1,36 @@
+package org.pahappa.pettycashapp.systems.petty_cash_app.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class NavigationBean {
+    private String selectedRequisitionsSection = "allRequisitions";
+    private String selectedBudgetLineSection = "currentBudgetLines";
+
+    public String getSelectedBudgetLineSection() {
+        return selectedBudgetLineSection;
+    }
+
+    public void setSelectedBudgetLineSection(String selectedBudgetLineSection) {
+        this.selectedBudgetLineSection = selectedBudgetLineSection;
+    }
+
+    public void changeBudgetLineSection(String BudgetLineSection) {
+        this.selectedBudgetLineSection = BudgetLineSection;
+    }
+
+    public String getSelectedRequisitionsSection() {
+        return selectedRequisitionsSection;
+    }
+
+    public void setSelectedRequisitionsSection(String selectedRequisitionsSection) {
+        this.selectedRequisitionsSection = selectedRequisitionsSection;
+    }
+
+    public void changeRequisitionsSections(String RequisitionSection) {
+        this.selectedRequisitionsSection = RequisitionSection;
+    }
+}
+
